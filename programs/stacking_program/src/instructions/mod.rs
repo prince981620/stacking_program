3ebdb8e62eb27d6f652f12932983ae4a78249405
prince981620 <prince981620@gmail.com>
2025-06-04 19:36:54 +0530

    adds un_stake nft instruction

--- programs/stacking_program/src/instructions/mod.rs
+++ programs/stacking_program/src/instructions/mod.rs
@@ -1,11 +1,17 @@
 pub mod initialize_config;
 pub mod initialize_user;
+
 pub mod stake_nft;
+pub mod unstake_nft;
+
 pub mod  stake_sol;
 pub mod stake_spl;
 
 pub use initialize_config::*;
 pub use initialize_user::*;
+
 pub use stake_nft::*;
+pub use unstake_nft::*;
+
 pub use stake_sol::*;
 pub use stake_spl::*;
